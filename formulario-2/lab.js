a27d21564df558a8f999cb08cde59f6d25694732
Jorge Villamil <jorgevillamil10233@hotmail.com>
2013-11-09 11:25:05 -0500

    clase 9 de noviembre

--- formulario-2/lab.js
+++ formulario-2/lab.js
@@ -1,17 +1,21 @@
-$(document).ready(function(){
+$(document).ready(function() {
 	$('.error').hide();
+	
 	$('.submit').click(function(event){
 		var data = $('.infobox').val();
-		var len=data.length;
+		var len = data.length;
 		var c;
-
-		for(var i =0; i<len; i++){
-			c = data.charAt(i).charCode(0);
-			if (c < 48 || c >57){
+		for(var i=0;i<len;i++)
+		{
+			c=data.charAt(i).charCodeAt(0);
+			if(c <48 || c >57)
+			{
 				$('.error').show();
 				event.preventDefault();
-				break;
-			} else {
+			break;
+			}
+			else
+			{
 				$('.error').hide();
 			}
 		}
